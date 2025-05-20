6c73b711f2203c1eb067af11193a183876b76a75
George Robinson <george.robinson@grafana.com>
2025-05-20 20:12:22 +0100

    Rename RateBucket to rateBucket

--- pkg/limits/http.go
+++ pkg/limits/http.go
@@ -48,8 +48,8 @@ func (s *IngestLimits) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 			// Calculate size only within the rate window
 			for _, bucket := range stream.RateBuckets {
-				if bucket.Timestamp >= rateWindowCutoff {
-					totalSize += bucket.Size
+				if bucket.timestamp >= rateWindowCutoff {
+					totalSize += bucket.size
 				}
 			}
 		}
