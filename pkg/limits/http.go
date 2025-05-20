b0b03eab8864465101f21059b4a760b5a8f23fc9
George Robinson <george.robinson@grafana.com>
2025-05-20 20:03:46 +0100

    chore: rename UsageStore to usageStore

--- pkg/limits/http.go
+++ pkg/limits/http.go
@@ -38,7 +38,7 @@ func (s *IngestLimits) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		response      httpTenantLimitsResponse
 	)
 
-	s.usage.All(func(tenantID string, _ int32, stream Stream) {
+	s.usage.all(func(tenantID string, _ int32, stream Stream) {
 		if tenantID != tenant {
 			return
 		}
